b1280842f5f803ded19ebde8733d928728028c7e
Hannes <hannes@rang.de>
2022-12-13 20:04:58 +0100

    reducing gas limit for redeem ticket

--- methods/redeem_ticket.js
+++ methods/redeem_ticket.js
@@ -4,7 +4,7 @@ const redeem_ticket = async (token_id, provider, ticket_demo_contract) => {
   const gasPrice = await get_gas_price(provider);
 
   const redeem_tx = await ticket_demo_contract.redeemNfticket(token_id, {
-    gasLimit: 1000000,
+    gasLimit: 200000,
     gasPrice: gasPrice,
   });
 
