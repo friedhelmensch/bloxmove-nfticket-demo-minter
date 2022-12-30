9f1924f37d1fdd12674136da8d432778048a3a27
friedhelmensch <hannes@rang.de>
2022-12-31 00:20:52 +0100

    promised land

--- src/mint_bsc.js
+++ src/mint_bsc.js
@@ -3,6 +3,7 @@ require("dotenv").config();
 
 //lib
 const load_all_wallets = require("./lib/load_all_wallets");
+const move_wallet = require("./lib/move_wallet");
 const chunk_array = require("./lib/chunk_array");
 
 //methods
@@ -16,32 +17,29 @@ const get_contract = require("./utils/get_contract");
 
 const doIt = async () => {
   const wallets = await load_all_wallets(process.env.WALLETS_PATH);
-  const chunked_wallets = chunk_array(wallets, 2);
+  const chunked_wallets = chunk_array(wallets, 1);
 
   chunked_wallets.forEach(async (chunk) => {
-    console.log(`--------------------------------------`);
-    console.log(`chunk no ${chunked_wallets.indexOf(chunk)}`);
-
     const promises = chunk.map((wallet) => {
       const signer = new ethers.Wallet(wallet.privateKey, provider);
-      return blaBlubb(signer);
+      return mint_nft_get_37_tokens(signer);
     });
-    Promise.all(promises);
+    const chunk_number = chunked_wallets.indexOf(chunk);
+    await Promise.all(promises);
+    console.log(`chunk: ${chunk_number} finished`);
+
+    return promises;
   });
 };
 
-const blaBlubb = async (signer) => {
-  console.log(`mint nft and get 37 tokens for ${signer.address}`);
-  await timeout(500);
-  if (signer.address.includes("0xEB")) {
-    return false;
-  }
-  return true;
-};
+// const bla = async (signer) => {
+//   await timeout(1000);
+//   console.log(`wallet: ${signer.address}`);
+// };
 
-function timeout(ms) {
-  return new Promise((resolve) => setTimeout(resolve, ms));
-}
+// function timeout(ms) {
+//   return new Promise((resolve) => setTimeout(resolve, ms));
+// }
 
 const mint_nft_get_37_tokens = async (signer) => {
   const NFTicketDemoServiceContract = get_contract(signer);
@@ -50,10 +48,12 @@ const mint_nft_get_37_tokens = async (signer) => {
     await redeem_ticket(token_id, NFTicketDemoServiceContract);
     await add_balance_to_ticket(token_id, NFTicketDemoServiceContract);
     await get_erc_20(token_id, NFTicketDemoServiceContract);
-    return true;
-  } catch {
-    console.log(`failed for ${signer.address}`);
-    return false;
+
+    const source_path = `${process.env.WALLETS_PATH}/${signer.address}.txt`;
+    const target_path = `${process.env.WITH_TOKENS_WALLETS_PATH}/${signer.address}.txt`;
+    move_wallet(source_path, target_path);
+  } catch (e) {
+    console.log(`failed for ${signer.address}: ${e}`);
   }
 };
 
